1ee98ac43e84322c4935a74ae0e9f231f8bad8ca
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2014-07-15 18:12:50 +0300

    make steps 1 and 3 of request

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-	
+	/* Custom radiobutton
+	========================================================================== */
+
+	$(".custom-radio").on("click", function() {
+		var $this = $(this),
+			groupName = $this.data("name");
+		$(".custom-radio[data-name=" + groupName + "]").removeClass("is-checked");
+		$this.toggleClass("is-checked");
+	});
 })
